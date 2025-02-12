cc36bb6da9533061d4853f06a025a595d9fff40d
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2025-02-12 09:36:46 -0300

    feat: configurado middleware cors, content-type e body-parser

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,7 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import app from './config/app'
+import env from './config/env'
 
-dotenv.config()
-
-const app = express()
-const port = process.env.PORT
+const port = env.port
 
 app.listen(port, () => {
     console.log(`Server Running on port http://localhost:${port}`)
